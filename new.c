a4001cfde98f020885b9689e2f0c70adfe175525
Muhammad Shahzad Laif <shahzad.aya4@gamil.com>
2025-07-01 11:59:38 +0500

    Write the code for printing the name in loop many time

---
+++ new.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char s[100];
+    printf("Enter your name: ");
+    fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0';
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Your name is: %s\n", s);
+    }
+}
