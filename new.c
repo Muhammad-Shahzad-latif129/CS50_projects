0eb2a673f870e54cd0c56ffc45b1c19bbd96b133
Muhammad Shahzad Laif <shahzad.aya4@gamil.com>
2025-07-01 23:18:14 +0500

    New feature added

--- new.c
+++ new.c
@@ -3,13 +3,25 @@
 int main(void)
 {
     char s[100];
+    int age;
     printf("Enter your name: ");
     fgets(s, sizeof(s), stdin);
     s[strcspn(s, "\n")] = '\0';
-    for (int i = 0; i < 5; i++)
+    printf("Enter your age: ");
+    scanf("%i", &age);
+    if (age < 18)
     {
-        printf("Your name is:- %s\n", s);
+        printf("%s you are not eligible for age %i\n", s, age);
     }
+    if(age <= 0)
+    {
+        printf("%s please enter the valid age.\n", s);
+    }
+    else
+    {
+        printf("%s you are eligible.\n", s);
+    }
+    
     printf("This is the end of Program.....\n");
     return 0;
 }
