98cb9216c77fee3e28e7eed791d837d6499529b5
Muhammad Shahzad latif <shahzad.aya4@gmail.com>
2025-07-17 19:09:44 +0500

    bit edit
    This program prompt for your name then say hello, <Your name>

--- ProblemSet_1/HelloItsMe.c
+++ ProblemSet_1/HelloItsMe.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+//For the strcspn
 #include<string.h>
 
 int main(void)
@@ -6,6 +7,7 @@ int main(void)
     char name[60];
     printf("What's your name? ");
     fgets(name, sizeof(name), stdin);
+    //Remove the \n from the buffer
     name[strcspn(name, "\n")] = '\0';
 
     printf("Hello, %s\n", name);
