55dc4a8b994f31944fcf2ee91a407cff606c0342
Muhammad Shahzad latif <shahzad.aya4@gmail.com>
2025-07-22 20:02:48 +0500

    Implement Scrabble scoring program for two players (CS50 Pset2)
    This program simulates a Scrabble scoring system. It allows two players to enter words, calculates the score for each based on letter values (similar to real Scrabble), and announces the winner or a tie. 
    
    Key features:
    - Accepts user input via `fgets` for safety.
    - Ignores case sensitivity (treats upper and lower case letters equally).
    - Calculates scores using an array that matches each letter to its Scrabble point value.
    
    Part of Harvard's CS50 Problem Set 2.

--- ProblemSet_2/scrabble.c
+++ ProblemSet_2/scrabble.c
@@ -1,1 +1,55 @@
+#include<ctype.h>
+#include<string.h>
+//It contains the strlen function to calculate the length of the string
+#include<stdio.h>
 
+int score(char word[]);
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2,4, 1, 8, 5,	1, 3, 1, 1,	3, 10, 1, 1, 1,	1, 4, 4, 8,	4, 10};
+int main(void)
+{
+    char word1[100];
+    printf("Player 1: ");
+    fgets(word1, sizeof(word1), stdin);
+    word1[strcspn(word1, "\n")] = '\0'; // Removing trailing newline
+
+    char word2[100];
+    printf("Player 2: ");
+    fgets(word2, sizeof(word2), stdin);
+    word2[strcspn(word2, "\n")] = '\0';
+
+    int score1 = score(word1);
+    int score2 = score(word2);
+
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!.\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!.\n");
+    }
+    else
+    {
+        printf("Tie!.\n");
+    }
+    
+}
+
+int score(char word[])
+{
+    int score = 0;
+
+    for (int i = 0, len = strlen(word); i < len; i++)
+    {
+        if (isupper(word[i]) )
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]) )
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+    return score;
+    
+}
